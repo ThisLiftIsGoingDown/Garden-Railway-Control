660673e26c2b03e7fe4d0cad3acd3a3836332ef7
HELLO64t <david.bartsch@bluewin.ch>
2021-06-04 10:15:56 +0200

    NEW
    NEw

---
+++ main.py
@@ -0,0 +1,45 @@
+import json
+
+
+
+def findPath(map, cazzo, culo):
+    explored = []
+    queue = [[cazzo]]
+    if cazzo == culo:
+        return "Same"
+    while queue:
+        path = queue.pop(0)
+        node = path[-1]
+        if node not in explored:
+            neighbours = map[node]
+            for neighbour in neighbours:
+                quickpass = list(path)
+                quickpass.append(neighbour)
+                queue.append(quickpass)
+                 
+                if neighbour == culo:
+                    print(quickpass)
+                    return quickpass
+            explored.append(node)
+ 
+    
+    print("No pass")
+    return 0
+ 
+
+mapdata = open("map.json","r")
+ 
+verify = mapdata.readlines(-1)
+mapdata.close()
+if verify == "":
+    pass
+     
+
+with open('map.json') as f:
+  map = json.load(f)
+
+
+with open("map.json", "w") as write_file:
+    json.dump(map, write_file)
+
+findPath(map, 'K', 'BK')
